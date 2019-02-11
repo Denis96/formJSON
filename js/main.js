39b4f3e633ff4031c4cf775c331fad690a1291f0
Denis <m8.denis.pereira@gmail.com>
2019-02-11 19:40:56 +0100

    Muchas funciones
    - SwapFuncition
    - patternOptionFunction
    - CRUD Question
    - Unbind

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 
 $(document).ready(function(){
 	var form = null;
+	var legend = null;
+	var label = null;
 	var addQuestion = null;
+	var delQuestion = null;
+	var swapQuestion = null;
+	var addOption = null;
 	var delOption = null;
 
 	updateEvents();
@@ -15,15 +20,29 @@ $(document).ready(function(){
 		legend = $("legend");
 		label = $("label");
 		addQuestion = $("#addQuestion");
+		delQuestion = $(".delQuestion");
+		swapQuestion = $(".swapQuestion");
+		addOption = $(".addOption");
 		delOption = $(".delOption");
 
-		addQuestion.click(generateQuestion);
-		delOption.click(removeOption);
-		legend.dblclick(changeName);
-		label.dblclick(changeName);
+		addQuestion.unbind();
+		delQuestion.unbind();
+		swapQuestion.unbind();
+		addOption.unbind();
+		delOption.unbind();
+		legend.unbind();
+		label.unbind();
+
+		addQuestion.click(generateQuestionFunction);
+		delQuestion.click(removeParentFunction);
+		swapQuestion.click(swapOptionsTypeFunction);
+		addOption.click(addOptionFunction);
+		delOption.click(removeParentFunction);
+		legend.dblclick(changeNameFunction);
+		label.dblclick(changeNameFunction);
 	}
 
-	function generateQuestion() {
+	function generateQuestionFunction() {
 		// create "fieldset" element with id
 		var fieldset = document.createElement("fieldset");
 		fieldset.setAttribute("id", ("question"+questionsCounter) );
@@ -34,53 +53,94 @@ $(document).ready(function(){
 		$(legend).append(legendContent);
 		$(fieldset).append(legend);
 
-
-		for (var i = 1 ; i <= 2 ; i++) {
+		// Create a "div" with 2 options
 			var div = document.createElement("div");
-
-			var buttonDelete = document.createElement("button");
-			buttonDelete.setAttribute("id", ("delete"+questionsCounter+"-"+i) );
-			buttonDelete.setAttribute("class", "delOption" );
-			var buttonDeleteContent = document.createTextNode("X");
-			$(buttonDelete).append(buttonDeleteContent);
-
-			var input = document.createElement("input");
-			input.setAttribute("type", "radio" );
-			input.setAttribute("name", "sample" );
-			input.setAttribute("id", ("opt"+i) );
-			input.setAttribute("name", ("opt"+i) );
-
-			var label = document.createElement("label");
-			input.setAttribute("for", ("opt"+i) );
-			var labelContent = document.createTextNode(("Option "+i));
-			$(label).append(labelContent);
-
-			var br = document.createElement("br");
-
-				$(div).append(buttonDelete);
-				$(div).append(input);
-				$(div).append(label);
-				$(div).append(br);
-				$(div).append(br);
+			for (var i = 1 ; i <= 2 ; i++) {
+				$(div).append( patternOptionFunction(i) );
+			}
 			$(fieldset).append(div);
-		}
+
+
+		var buttonAdd = document.createElement("button");
+		buttonAdd.setAttribute("class", "addOption" );
+		var buttonAddContent = document.createTextNode("add");
+		$(buttonAdd).append(buttonAddContent);
+
+		var buttonDelete = document.createElement("button");
+		buttonDelete.setAttribute("class", "delQuestion" );
+		var buttonDeleteContent = document.createTextNode("del");
+		$(buttonDelete).append(buttonDeleteContent);
+
+		var buttonSwap = document.createElement("button");
+		buttonSwap.setAttribute("class", "swapQuestion" );
+		var buttonSwapContent = document.createTextNode("swap");
+		$(buttonSwap).append(buttonSwapContent);
+		
+		var br = document.createElement("br");
+
+		$(fieldset).append(br);
+		$(fieldset).append(buttonAdd);
+		$(fieldset).append(buttonDelete);
+		$(fieldset).append(buttonSwap);
 
 		$(form).append(fieldset);
+
+		questionsCounter++;
 
 		updateEvents();
 
 	}
 
-	function removeOption() {
+	function patternOptionFunction(i) {
+		var div = document.createElement("div");
+
+		var buttonDelete = document.createElement("button");
+		buttonDelete.setAttribute("class", "delOption" );
+		var buttonDeleteContent = document.createTextNode("X");
+		$(buttonDelete).append(buttonDeleteContent);
+
+		var input = document.createElement("input");
+		input.setAttribute("type", "radio" );
+		input.setAttribute("name", "sample" );
+		input.setAttribute("id", ("opt"+questionsCounter+"-"+i) );
+		input.setAttribute("name", ("question"+questionsCounter) );
+
+		var label = document.createElement("label");
+		label.setAttribute("for", ("opt"+questionsCounter+"-"+i) );
+		var labelContent = document.createTextNode(("Option "+i));
+		$(label).append(labelContent);
+
+		var br = document.createElement("br");
+
+			$(div).append(buttonDelete);
+			$(div).append(input);
+			$(div).append(label);
+			$(div).append(br);
+
+		return div;
+	}
+
+	function addOptionFunction() {
+		$(this).parent().children("div").append( patternOptionFunction(3) );
+	}
+
+	function removeParentFunction() {
 		$(this).parent().remove();
 	}
 
-	function changeName() {
+	function swapOptionsTypeFunction() {
+		var options = $(this).parent().children("div");
+		if ( options.first().children("div").first().children("input").first().attr("type") == "radio" ) {
+			options.children("div").children("input").attr( "type","checkbox" );
+		} else {
+			options.children("div").children("input").attr( "type","radio" );
+		}
+	}
 
+	function changeNameFunction() {
 		var content = prompt("", $(this).html());
 		if (content != null) {
 			$(this).html(content);
 		}
-		console.log($(this).html());
 	}
 });
