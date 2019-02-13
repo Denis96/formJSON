231c2fc364684d5d3c4bf8ee738b01676dfc4519
Denis <m8.denis.pereira@gmail.com>
2019-02-13 20:13:15 +0100

    All + finished
    + Comments
    + Change inputs patternOptionFunction
    + JSON generate

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){
 	var form = null;
+	var formName = null;
 	var legend = null;
 	var label = null;
 	var addQuestion = null;
@@ -12,65 +13,73 @@ $(document).ready(function(){
 
 	updateEvents();
 
-	var questionsCounter = 0;
+	var questionsCounter = 1;
 
 
 
 	function updateEvents() {
-		form = $("#form");
-		legend = $("legend");
-		label = $("label");
-		addQuestion = $("#addQuestion");
-		delQuestion = $(".delQuestion");
-		swapQuestion = $(".swapQuestion");
-		addOption = $(".addOption");
-		delOption = $(".delOption");
-		generateJSON = $("#generateJSON");
+		// Search all elements
+			form = $("#form");
+			formName = $("#formName");
+			legend = $("legend");
+			label = $("label");
+			addQuestion = $("#addQuestion");
+			delQuestion = $(".delQuestion");
+			swapQuestion = $(".swapQuestion");
+			addOption = $(".addOption");
+			delOption = $(".delOption");
+			generateJSON = $("#generateJSON");
 
-		addQuestion.unbind();
-		delQuestion.unbind();
-		swapQuestion.unbind();
-		addOption.unbind();
-		delOption.unbind();
-		legend.unbind();
-		label.unbind();
-		generateJSON.unbind();
+		//	Delete all assigned events 
+			formName.unbind();
+			legend.unbind();
+			label.unbind();
+			addQuestion.unbind();
+			delQuestion.unbind();
+			swapQuestion.unbind();
+			addOption.unbind();
+			delOption.unbind();
+			generateJSON.unbind();
 
-		addQuestion.click(generateQuestionFunction);
-		delQuestion.click(removeParentFunction);
-		swapQuestion.click(swapOptionsTypeFunction);
-		addOption.click(addOptionFunction);
-		delOption.click(removeParentFunction);
-		legend.dblclick(changeNameFunction);
-		label.dblclick(changeNameFunction);
-		generateJSON.click(testDownload);
+		//	Returns assign events
+			formName.dblclick(changeNameFunction);
+			legend.dblclick(changeNameFunction);
+			label.dblclick(changeNameFunction);
+			addQuestion.click(generateQuestionFunction);
+			delQuestion.click(removeParentFunction);
+			swapQuestion.click(swapOptionsTypeFunction);
+			addOption.click(addOptionFunction);
+			delOption.click(removeParentFunction);
+			generateJSON.click(prepareDownload);
 	}
 
 	function generateQuestionFunction() {
-		// create "fieldset" element with id
-		var fieldset = document.createElement("fieldset");
-		fieldset.setAttribute("id", ("question"+questionsCounter) );
+		//	Create "fieldset" element with id
+			var fieldset = document.createElement("fieldset");
+			fieldset.setAttribute("id", ("question"+questionsCounter) );
 
-		var legend = document.createElement("legend");
-		var legendContent = document.createTextNode("Sample");
+			var legend = document.createElement("legend");
+			var legendContent = document.createTextNode("Question "+questionsCounter);
 
-		var hiddenQuestion = document.createElement("input");
-		hiddenQuestion.setAttribute("type", "hidden" );
-		hiddenQuestion.setAttribute("id", "questionId" );
-		hiddenQuestion.setAttribute("name", "questionId" );
-		hiddenQuestion.setAttribute("value", questionsCounter );
 
-		var hiddenOptionsNumber = document.createElement("input");
-		hiddenOptionsNumber.setAttribute("type", "hidden" );
-		hiddenOptionsNumber.setAttribute("id", "optionsNumber" );
-		hiddenOptionsNumber.setAttribute("name", "optionsNumber" );
-		hiddenOptionsNumber.setAttribute("value", 2 );
+		//	Create 3 hidden inputs for control the question
+			var hiddenQuestion = document.createElement("input");
+			hiddenQuestion.setAttribute("type", "hidden" );
+			hiddenQuestion.setAttribute("class", "questionId" );
+			hiddenQuestion.setAttribute("name", "questionId" );
+			hiddenQuestion.setAttribute("value", questionsCounter );
 
-		var hiddenOptionsType = document.createElement("input");
-		hiddenOptionsType.setAttribute("type", "hidden" );
-		hiddenOptionsType.setAttribute("id", "optionsType" );
-		hiddenOptionsType.setAttribute("name", "optionsType" );
-		hiddenOptionsType.setAttribute("value", "radio" );
+			var hiddenOptionsNumber = document.createElement("input");
+			hiddenOptionsNumber.setAttribute("type", "hidden" );
+			hiddenOptionsNumber.setAttribute("class", "optionsNumber" );
+			hiddenOptionsNumber.setAttribute("name", "optionsNumber" );
+			hiddenOptionsNumber.setAttribute("value", 2 );
+
+			var hiddenOptionsType = document.createElement("input");
+			hiddenOptionsType.setAttribute("type", "hidden" );
+			hiddenOptionsType.setAttribute("class", "optionsType" );
+			hiddenOptionsType.setAttribute("name", "optionsType" );
+			hiddenOptionsType.setAttribute("value", "radio" );
 		
 		$(legend).append(legendContent);
 		$(fieldset).append(legend);
@@ -78,28 +87,29 @@ $(document).ready(function(){
 		$(fieldset).append(hiddenOptionsNumber);
 		$(fieldset).append(hiddenOptionsType);
 
-		// Create a "div" with 2 options
+		//	Create a "div" with 2 options
 			var div = document.createElement("div");
+			div.setAttribute("class", "options" );
 			for (var i = 1 ; i <= 2 ; i++) {
-				$(div).append( patternOptionFunction(i, questionsCounter) );
+				$(div).append( patternOptionFunction(i, questionsCounter, "radio") );
 			}
 			$(fieldset).append(div);
 
+		//	Create a Add, Delete and Swap buttons
+			var buttonAdd = document.createElement("button");
+			buttonAdd.setAttribute("class", "addOption" );
+			var buttonAddContent = document.createTextNode("add");
+			$(buttonAdd).append(buttonAddContent);
 
-		var buttonAdd = document.createElement("button");
-		buttonAdd.setAttribute("class", "addOption" );
-		var buttonAddContent = document.createTextNode("add");
-		$(buttonAdd).append(buttonAddContent);
+			var buttonDelete = document.createElement("button");
+			buttonDelete.setAttribute("class", "delQuestion" );
+			var buttonDeleteContent = document.createTextNode("del");
+			$(buttonDelete).append(buttonDeleteContent);
 
-		var buttonDelete = document.createElement("button");
-		buttonDelete.setAttribute("class", "delQuestion" );
-		var buttonDeleteContent = document.createTextNode("del");
-		$(buttonDelete).append(buttonDeleteContent);
-
-		var buttonSwap = document.createElement("button");
-		buttonSwap.setAttribute("class", "swapQuestion" );
-		var buttonSwapContent = document.createTextNode("swap");
-		$(buttonSwap).append(buttonSwapContent);
+			var buttonSwap = document.createElement("button");
+			buttonSwap.setAttribute("class", "swapQuestion" );
+			var buttonSwapContent = document.createTextNode("swap");
+			$(buttonSwap).append(buttonSwapContent);
 		
 		var br = document.createElement("br");
 
@@ -116,42 +126,48 @@ $(document).ready(function(){
 
 	}
 
-	function patternOptionFunction(i, currentQuestion) {
+	// Returns a div with 2 options
+	function patternOptionFunction(number, currentQuestion, optionType) {
 		var div = document.createElement("div");
 
-		var buttonDelete = document.createElement("button");
-		buttonDelete.setAttribute("class", "delOption" );
-		var buttonDeleteContent = document.createTextNode("X");
-		$(buttonDelete).append(buttonDeleteContent);
+		//	Delete button
+			var buttonDelete = document.createElement("button");
+			buttonDelete.setAttribute("class", "delOption" );
+			var buttonDeleteContent = document.createTextNode("X");
+			$(buttonDelete).append(buttonDeleteContent);
 
-		var input = document.createElement("input");
-		input.setAttribute("type", "radio" );
-		input.setAttribute("id", ("opt"+currentQuestion+"-"+i) );
-		input.setAttribute("name", ("question"+currentQuestion) );
+		//	Input (radio or checkbox)
+			var input = document.createElement("input");
+			input.setAttribute("type", optionType );
+			input.setAttribute("id", ("opt"+currentQuestion+"-"+number) );
+			input.setAttribute("name", ("question"+currentQuestion) );
 
-		var label = document.createElement("label");
-		label.setAttribute("for", ("opt"+currentQuestion+"-"+i) );
-		var labelContent = document.createTextNode(("Option "+i));
-		$(label).append(labelContent);
+		//	Label for input
+			var label = document.createElement("label");
+			label.setAttribute("for", ("opt"+currentQuestion+"-"+number) );
+			var labelContent = document.createTextNode(("New option"));
+			$(label).append(labelContent);
 
 		var br = document.createElement("br");
 
-			$(div).append(buttonDelete);
-			$(div).append(input);
-			$(div).append(label);
-			$(div).append(br);
+		$(div).append(buttonDelete);
+		$(div).append(input);
+		$(div).append(label);
+		$(div).append(br);
 
 		return div;
 	}
 
 	function addOptionFunction() {
-		var currentQuestion = $(this).parent().children("#questionId").attr("value");
+		//	Info for greate a option
+			var currentQuestion = $(this).parent().children(".questionId").attr("value");
+			var optionType = $(this).parent().children(".optionsType").attr("value");
+
+			var optionsNumber = parseInt( $(this).parent().children(".optionsNumber").attr("value") )+1;
+			$(this).parent().children(".optionsNumber").attr("value", optionsNumber);
 
 
-		var optionsNumber = parseInt( $(this).parent().children("#optionsNumber").attr("value") )+1;
-		$(this).parent().children("#optionsNumber").attr("value", optionsNumber);
-
-		$(this).parent().children("div").append( patternOptionFunction(optionsNumber, currentQuestion) );
+		$(this).parent().children("div").append( patternOptionFunction(optionsNumber, currentQuestion, optionType) );
 		updateEvents();
 	}
 
@@ -161,11 +177,12 @@ $(document).ready(function(){
 
 	function swapOptionsTypeFunction() {
 		var options = $(this).parent();
-		if ( options.children("#optionsType").attr("value") == "radio" ) {
+		if ( options.children(".optionsType").attr("value") == "radio" ) {
 			options.children("div").children("div").children("input").attr( "type","checkbox" );
-		} else { /////////////////////////////// mirar
-			console.log(options.children("div").children("div").children("input").attr( "type"));
+			options.children(".optionsType").attr("value", "checkbox");
+		} else {
 			options.children("div").children("div").children("input").attr( "type","radio" );
+			options.children(".optionsType").attr("value","radio");
 		}
 	}
 
@@ -175,19 +192,67 @@ $(document).ready(function(){
 			$(this).html(content);
 		}
 	}
-	function testDownload() {
-		download("myfilename"+".json", "nepe");
-	}
-	function download(filename, text) {
-		var element = document.createElement('a');
-		element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-		element.setAttribute('download', filename);
 
-		element.style.display = 'none';
-		document.body.appendChild(element);
 
-		element.click();
+	//////	JSON Funtion 
+		function generateJSONFunction() {
+			var content = "{";
+				content += "\"name\": \""+ $(form).children("#formName").text() +"\",";
+				content += "\"rows\": [ {";
+					content += "\"idTablet\": \"TABLET8\",";
+					content += "\"version\": \"1.1\",";
+					content += "\"questions\": [ {";
+						content += "\"question\": \" \",";
+						content += "\"type\": \"HOME\"}";
 
-		document.body.removeChild(element);
-	}
+						//	opcions from the form
+						$(form).children("fieldset").each(function( index ) {
+							content += ", {";
+							content += "\"question\": \""+ $(this).children("legend").text() +"\",";
+							content += "\"type\": \""
+								switch ( $(this).children(".optionsType").val() ) {
+									case "radio":		content += "OPTIONS";			break;
+									case "checkbox":	content += "MULTIPLE_OPTIONS";	break;
+									default:	console.log("ERROR: unexpected form type");
+												console.log($(this).children(".optionsType")); ;
+								}
+								content += "\",";
+							content += "\"options\": [";
+							content += "\""+ $(this).children(".options").children("div:first-child").children("label").text() +"\"";
+							$(this).children(".options").children("div").each(function( index ) {
+								index != 0 ? content += ", \""+ $(this).children("label").text() +"\"" : "";
+							});
+							content += "] }";
+						});
+
+						//	final log
+						content += ", {";
+						content += "\"questions\": \"Tot Correcte?\",";
+						content += "\"type\": \"FINAL_LOG\"";
+						content += "} ]";
+					content += "} ]";
+				content += "}";
+			return content;
+		}
+
+
+	//////	Functions for download 
+		function prepareDownload() {
+			var jsonName = $("#jsonName").val();
+			var contentJson = generateJSONFunction();
+			console.log(contentJson);
+			jsonName!="" ? download(jsonName+".json", contentJson) : download("your_json.json", contentJson);
+		}
+		function download(filename, text) {
+			var element = document.createElement('a');
+			element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+			element.setAttribute('download', filename);
+
+			element.style.display = 'none';
+			document.body.appendChild(element);
+
+			element.click();
+
+			document.body.removeChild(element);
+		}
 });
